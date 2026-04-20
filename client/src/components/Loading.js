13c07c54aedfee20e1a2100f2aead2de34703f6e
joseagustinalvarenga <agusalvarenga44@gmail.com>
2026-04-20 11:26:27 -0300

    Branded loading animation: Jumping TurnoHub letters with dual-ring spinner

--- client/src/components/Loading.js
+++ client/src/components/Loading.js
@@ -2,10 +2,25 @@ import React from 'react';
 import styles from './Loading.module.css';
 
 export default function Loading() {
+  const brand = "TurnoHub".split("");
+
   return (
     <div className={styles.container}>
-      <div className={styles.spinner}></div>
-      <p className={styles.text}>Cargando...</p>
+      <div className={styles.wrapper}>
+        <div className={styles.spinner}></div>
+        <div className={styles.brand}>
+          {brand.map((letter, index) => (
+            <span 
+              key={index} 
+              className={styles.letter} 
+              style={{ animationDelay: `${index * 0.1}s` }}
+            >
+              {letter}
+            </span>
+          ))}
+        </div>
+      </div>
+      <p className={styles.subtext}>Preparando todo para vos...</p>
     </div>
   );
 }
